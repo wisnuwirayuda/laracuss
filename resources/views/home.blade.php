b505e8101e0d2a2c8ea3b2e6d609d6ab5158d01d
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2024-04-14 22:08:48 +0700

    Membuat UI Home - Hero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,5 +48,20 @@
               </div>
             </div>
         </nav>
+        <section class="container hero">
+          <div class="row align-items-center h-100">
+            <div class="col-12 col-lg-6">
+              <h1>The Laravel<br />Community Forum</h1>
+              <p class="mb-4">
+                Empowering the Laravel community to connect, share and learn.
+              </p>
+              <a class="btn btn-primary me-2 mb-2 mb-lg-0" href="#">Sign Up</a>
+              <a class="btn btn-secondary mb-2 mb-lg-0" href="#">Join Discussions</a>
+            </div>
+            <div class="col-12 col-lg-6 h-315px order-first order-lg-last mb-3 mb-lg-0">
+              <img class="hero-image float-lg-end" src="{{ url("assets/img/hero.png") }}" alt="hero">
+            </div>
+          </div>
+        </section>
     </body>
 </html>
