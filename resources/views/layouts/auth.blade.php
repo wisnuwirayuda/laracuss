4c6f1299698f54f8bbe94d90b103e37d97e7c7c3
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2024-04-16 11:05:19 +0700

    Membuat Reusable Komponen

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('partials.head')
+    </head>
+    <body class="antialiased">
+        @include('partials.nav')
+
+        @yield('body')
+
+        @yield('before-script')
+        
+        @include('partials.script')
+
+        @yield('after-script')
+    </body>
+</html>
