3a81e8363ec02e5a78a1f16d13ed9c9cac740d00
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2024-05-11 08:05:32 +0700

    Membuat UI Edit Profile Page

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,7 @@ Route::get('answers/1', function () {
 Route::get('users/wisnuwirayuda', function () {
     return view('pages.users.show');
 })->name('users.show');
+
+Route::get('users/wisnuwirayuda/edit', function () {
+    return view('pages.users.form');
+})->name('users.edit');
