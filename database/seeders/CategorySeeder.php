95d88465e2cff9284d5d287ed473ed7dca429f86
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2024-06-08 21:21:41 +0700

    Buat Seeder Untuk Category Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            [
+                'slug' => 'code-review',
+                'name' => 'Code Review',
+            ],
+            [
+                'slug' => 'eloquent',
+                'name' => 'Eloquent',
+            ],
+            [
+                'slug' => 'facade',
+                'name' => 'Facade',
+            ],
+            [
+                'slug' => 'general',
+                'name' => 'General',
+            ],
+            [
+                'slug' => 'javascript',
+                'name' => 'Javascript',
+            ],
+            [
+                'slug' => 'mix',
+                'name' => 'Mix',
+            ],
+            [
+                'slug' => 'query-builder',
+                'name' => 'Query Builder',
+            ],
+            [
+                'slug' => 'servers',
+                'name' => 'Servers',
+            ],
+            [
+                'slug' => 'testing',
+                'name' => 'Testing',
+            ],
+            [
+                'slug' => 'vite',
+                'name' => 'Vite',
+            ],
+        ]);
+    }
+}
