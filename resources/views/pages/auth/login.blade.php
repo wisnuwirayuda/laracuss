2ee3437a80a2a73944e0e8e281eac0644a73211c
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2024-06-08 22:18:25 +0700

    Membuat & Integrasi Halaman Login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -9,20 +9,30 @@
                         <img class="h-32px" src="{{ url('assets/img/logo-blue.png') }}" alt="Laracuss-Logo">
                     </a>
                     <div class="card mb-5">
-                        <form action="#" method="POST">
+                        <form action="{{ route('auth.login.login') }}" method="POST">
+                            @csrf
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" autocomplete="off" autofocus>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror @error('credentials') is-invalid @enderror" id="email" name="email" placeholder="name@example.com" value="{{ old('email') }}" autocomplete="off" autofocus>
+                                @error('email')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                                @error('credentials')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
                                 <div class="input-group">
-                                    <input type="password" class="form-control border-end-0 pe-0 rounded-0 rounded-start" id="password" name="password" placeholder="******">
-                                    <span class="input-group-text bg-white border-start-0 pe-auto">
+                                    <input type="password" class="form-control border-end-0 pe-0 rounded-0 rounded-start @error('password') is-invalid @enderror" id="password" name="password" placeholder="******">
+                                    <span class="input-group-text bg-white border-start-0 pe-auto @error('password') border-danger rounded-end @enderror">
                                         <a href="javascript:;" id="password-toggle">
                                             <img src="{{ url("assets/img/eye-slash.png") }}" alt="Password Toggle" class="h-18px" id="password-toggle-image">
                                         </a>
                                     </span>
+                                    @error('password')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="mb-3 d-grid">
